611d0ccc4cd72b6cd10697e4f21bdbc7fe9c9dc2
Nikos Koutelidis <nikoutel@gmail.com>
2019-05-24 16:05:26 +0200

    Move array helper from value to trait

--- src/HelionConfigValue.php
+++ src/HelionConfigValue.php
@@ -3,8 +3,10 @@
 
 namespace Nikoutel\HelionConfig;
 
-class HelionConfigValue
+final class HelionConfigValue
 {
+    use arrayTrait;
+
     public $name;
     public $value;
     public $attributes;
@@ -21,32 +23,5 @@ class HelionConfigValue
 
     public function asArrayFlat() {
         return $this->flattenArray($this->castToArray($this));
-    }
-
-    private function castToArray($helionConfigValueObject) {
-        if (is_scalar($helionConfigValueObject)) {
-            return $helionConfigValueObject;
-        }
-        return array_map(array($this, 'castToArray'), array_filter((array)$helionConfigValueObject));
-    }
-
-    private function flattenArray($helionConfigArray, $sectionPrefix = '') {
-        $sectionSeparator = '.';
-        $helionConfigArrayFlatt = array();
-        foreach ($helionConfigArray as $key => $value) {
-            if ($key == 'name') {
-                continue;
-            }
-            if (is_array($value)) {
-                if ($key == 'value') {
-                    $helionConfigArrayFlatt = $helionConfigArrayFlatt + $this->flattenArray($value, $sectionPrefix);
-                } else {
-                    $helionConfigArrayFlatt = $helionConfigArrayFlatt + $this->flattenArray($value, $sectionPrefix . $key . $sectionSeparator);
-                }
-            } else {
-                $helionConfigArrayFlatt[rtrim($sectionPrefix, $sectionSeparator)] = $value;
-            }
-        }
-        return $helionConfigArrayFlatt;
     }
 }

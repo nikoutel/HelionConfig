dc40061efbf4d7051bf1f725b9e10d12ecc36781
Nikos Koutelidis <nikoutel@gmail.com>
2019-05-24 16:02:52 +0200

    Add asArrayFlat functionality

--- src/HelionConfigValue.php
+++ src/HelionConfigValue.php
@@ -19,10 +19,34 @@ class HelionConfigValue
         return $this->castToArray($this);
     }
 
+    public function asArrayFlat() {
+        return $this->flattenArray($this->castToArray($this));
+    }
+
     private function castToArray($helionConfigValueObject) {
         if (is_scalar($helionConfigValueObject)) {
             return $helionConfigValueObject;
         }
-        return array_map(array($this, 'castToArray'), (array)$helionConfigValueObject);
+        return array_map(array($this, 'castToArray'), array_filter((array)$helionConfigValueObject));
+    }
+
+    private function flattenArray($helionConfigArray, $sectionPrefix = '') {
+        $sectionSeparator = '.';
+        $helionConfigArrayFlatt = array();
+        foreach ($helionConfigArray as $key => $value) {
+            if ($key == 'name') {
+                continue;
+            }
+            if (is_array($value)) {
+                if ($key == 'value') {
+                    $helionConfigArrayFlatt = $helionConfigArrayFlatt + $this->flattenArray($value, $sectionPrefix);
+                } else {
+                    $helionConfigArrayFlatt = $helionConfigArrayFlatt + $this->flattenArray($value, $sectionPrefix . $key . $sectionSeparator);
+                }
+            } else {
+                $helionConfigArrayFlatt[rtrim($sectionPrefix, $sectionSeparator)] = $value;
+            }
+        }
+        return $helionConfigArrayFlatt;
     }
 }

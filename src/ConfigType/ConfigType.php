666d5f7c2aebca6d77bb8194c8ee11bdee470870
Nikos Koutelidis <nikoutel@gmail.com>
2019-05-24 14:08:33 +0200

    Error handling

--- src/ConfigType/ConfigType.php
+++ src/ConfigType/ConfigType.php
@@ -8,13 +8,21 @@ class ConfigType
 
     public function getConfigString($configSrc) {
         if (file_exists($configSrc)) {
-            return file_get_contents($configSrc);
+            $fileContents = file_get_contents($configSrc);
+            if ($fileContents === false) {
+                throw new \ErrorException(error_get_last()["message"]);
+            }
+            return $fileContents;
         } elseif (filter_var($configSrc, FILTER_VALIDATE_URL)) {
             $curlSession = curl_init();
             curl_setopt($curlSession, CURLOPT_URL, $configSrc);
             curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
             curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true); //@todo allow additional external CURL options
+            curl_setopt($curlSession, CURLOPT_FAILONERROR, true);
             $result = curl_exec($curlSession);
+            if (curl_error($curlSession)) {
+                throw new \ErrorException(curl_error($curlSession));
+            }
             curl_close($curlSession);
             return $result;
         } else {
--- src/ConfigType/INI.php
+++ src/ConfigType/INI.php
@@ -7,15 +7,26 @@ use Nikoutel\HelionConfig\HelionConfigValue;
 class INI extends ConfigType implements ConfigTypeInterface
 {
 
-
     public function getConfig($configSrc) {
-        $configString = $this->getConfigString($configSrc);
-        $helionConfig = $this->parseConfigString($configString);
+
+        try {
+            $configString = $this->getConfigString($configSrc);
+        } catch (\ErrorException $e) {
+            return new HelionConfigValue('Error', $e->getMessage());
+        }
+        try {
+            $helionConfig = $this->parseConfigString($configString);
+        } catch (\UnexpectedValueException $e) {
+            return new HelionConfigValue('Error', $e->getMessage());
+        }
         return $helionConfig;
     }
 
     public function parseConfigString($configString) {
         $iniString = parse_ini_string($configString, true);
+        if ($iniString === false || empty($iniString)) {
+            throw new \UnexpectedValueException('INI format error!');
+        }
         $helionConfigValue = $this->toHelionConfigValue($iniString, $this->configRootName);
         return $helionConfigValue;
     }

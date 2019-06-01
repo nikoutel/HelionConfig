97fffbaf2c103482fac299f34bacd0c0d35bd778
Nikos Koutelidis <nikoutel@gmail.com>
2019-06-01 12:58:55 +0200

    Implement the XML configuration file parsing

--- src/ConfigType/XML.php
+++ src/ConfigType/XML.php
@@ -2,9 +2,67 @@
 
 namespace Nikoutel\HelionConfig\ConfigType;
 
+use Nikoutel\HelionConfig\HelionConfigValue;
+
 class XML extends ConfigType implements ConfigTypeInterface
 {
-    public function getConfig($configSrc) {
-        return 'XML getConfig() called!';
+    public function parseConfigString($configString) {
+        $simpleXMLElement = new \SimpleXMLElement($configString);
+        $helionConfigValue = $this->toHelionConfigValue($simpleXMLElement);
+        return $helionConfigValue;
     }
+
+    private function toHelionConfigValue(\SimpleXMLElement $simpleXMLElement) {
+        $children = array();
+        $attributes = array();
+
+        $name = (string)$simpleXMLElement->getName();
+        $text = (string)$simpleXMLElement;
+        if (empty($text)) {
+            $text = NULL;
+        }
+
+        $namespace = $simpleXMLElement->getDocNamespaces(true);
+        $namespace[NULL] = NULL;
+
+        if (is_object($simpleXMLElement)) {
+            foreach ($namespace as $nsName => $nsURI) {
+
+                $simpleXMLElementAttributes = $simpleXMLElement->attributes($nsName, true);
+                foreach ($simpleXMLElementAttributes as $attributeName => $attributeValue) {
+                    $attributeNameStr = (string)$attributeName;
+                    $attributeValueStr = (string)$attributeValue;
+                    if (!empty($nsName)) {
+                        $attributeNameStr = $nsName . ':' . $attributeNameStr;
+                    }
+                    $attributes[$attributeNameStr] = $attributeValueStr;
+                }
+
+                $simpleXMLElementChildren = $simpleXMLElement->children($nsName, true);
+                foreach ($simpleXMLElementChildren as $childName => $child) {
+                    $childName = (string)$childName;
+                    if (!empty($nsName)) {
+                        $childName = $nsName . ':' . $childName;
+                    }
+                    if (array_key_exists($childName, $children)) {
+                        $childClone = clone $children[$childName];
+                        if (!is_array($children[$childName]->value) || !array_key_exists(0, $children[$childName]->value)) { // UGLY HACK!!
+                            $children[$childName]->value = array($childClone);
+                        }
+                        $children[$childName]->value[] = $this->toHelionConfigValue($child);
+                    } else {
+                        $children[$childName] = $this->toHelionConfigValue($child);
+                    }
+                }
+            }
+        }
+        if (!empty($children)) {
+            $value = $children;
+        } else {
+            $value = $text;
+        }
+        $returnVal = new HelionConfigValue($name, $value, $attributes);
+        return $returnVal;
+    }
+
 }

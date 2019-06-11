7f9212f4a7933b660ccc25071a9ae55dd646ea7d
Nikos Koutelidis <nikoutel@gmail.com>
2019-06-11 12:18:13 +0200

    Implement the generic CONF configuration file parsing

--- src/ConfigType/Conf.php
+++ src/ConfigType/Conf.php
@@ -2,9 +2,94 @@
 
 namespace Nikoutel\HelionConfig\ConfigType;
 
+use Nikoutel\HelionConfig\HelionConfigValue;
+
 class Conf extends ConfigType implements ConfigTypeInterface
 {
-    public function getConfig($configSrc) {
-        return 'Conf getConfig() called!';
+    /**
+     * Parses the Generic type configuration string and returns
+     * the equivalent Helion configuration value object
+     *
+     * @param string $configString
+     * @return HelionConfigValue
+     */
+    public function parseConfigString($configString) {
+
+        $symb['sectionStart'] = "[";
+        $symb['sectionEnd'] = "]";
+        $symb['equals'] = ':';
+        $symb['multiLineSeparator'] = '\\';
+        $symb['commentStart'] = '#';
+
+        $symb = $this->prep($symb);
+
+        $previousLine = '';
+        $confArrayResult = array();
+        $section = null;
+
+        $configArray = preg_split('/$\R?^/m', $configString); // Split $configString by new line
+        foreach ($configArray as $configLine) {
+            if (!preg_match("/^\s*" . $symb['commentStart'] . "/", $configLine) && preg_match("/^\s*(.*)" . $symb['multiLineSeparator'] . "\s*$/", $configLine, $configMatches)) {
+                $previousLine .= $configMatches[1] . ' ';
+                continue;
+            }
+            if (!empty($previousLine)) {
+                $configLine = $previousLine . trim($configLine);
+                $previousLine = '';
+            }
+            if (preg_match("/^\s*([\w-]+)\s*" . $symb['equals'] . "\s*((.*?)|)\s*$/", $configLine, $configMatches)) {
+                if (!isset($section)) {
+                    $confArrayResult[$configMatches[1]] = $configMatches[2];
+                } else {
+                    $confArrayResult[$section][$configMatches[1]] = $configMatches[2];
+                }
+            }
+            if (preg_match("/^\s*" . $symb['sectionStart'] . "\s*(.*)\s*" . $symb['sectionEnd'] . "\s*$/", $configLine, $configMatches)) {
+                $section = $configMatches[1];
+                $confArrayResult[$section] = array();
+            }
+        }
+
+        $helionConfigValue = $this->toHelionConfigValue($confArrayResult, $this->configRootName);
+        return $helionConfigValue;
+    }
+
+    /**
+     * Prepares pattern part for regex
+     *
+     * @param array $symbols
+     * @return array
+     */
+    private function prep(array $symbols){
+        $symbolsPrepd = array();
+        foreach ($symbols as $key => $symbol){
+            $symbolsPrepd[$key] = preg_replace('/(.)/', '\\\${1}', $symbol);
+        }
+        return $symbolsPrepd;
+    }
+    
+    /**
+     * Generates the Helion configuration value object
+     *
+     * @param array $confArrayResult
+     * @param string $name
+     * @return HelionConfigValue
+     */
+    private function toHelionConfigValue(array $confArrayResult, $name) {
+        $value = array();
+        foreach ($confArrayResult as $key => $leaf) {
+            if (is_array($leaf)) {
+                $value[$key] = $this->toHelionConfigValue($leaf, $key);
+            } else {
+                if (isset($confArrayResult[0])) {
+                    $keyName = $name . '.' . $key;
+                } else {
+                    $keyName = $key;
+                }
+                $value[$key] = new HelionConfigValue($keyName, $leaf);
+            }
+        }
+        $helionConfigValue = new HelionConfigValue($name, $value);
+        return $helionConfigValue;
     }
 }

61332c15f4770c42ba580e5ff8bf14d480d48e34
Nikos Koutelidis <nikoutel@gmail.com>
2019-06-12 11:15:49 +0200

    Add license header to Conf.php

--- src/ConfigType/Conf.php
+++ src/ConfigType/Conf.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ *
+ * Conf: generic configuration type parser
+ *
+ *
+ * @package HelionConfig
+ * @author Nikos Koutelidis nikoutel@gmail.com
+ * @copyright 2019 Nikos Koutelidis
+ * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ *
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
 
 namespace Nikoutel\HelionConfig\ConfigType;
 
